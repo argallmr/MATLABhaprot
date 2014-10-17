1cd235f4935f8f51b738f68a00f8bfbcf0baec74
argallmr <argallmr@gmail.com>
2014-10-17 10:37:52 -0400

    Removed ISO from ISOdate2mjd.

--- date2mjd.m
+++ date2mjd.m
@@ -1,22 +1,22 @@
 %--------------------------------------------------------------------------
 % NAME
-%   ISOdate2mjd
+%   date2mjd
 %
 % PURPOSE
 %   Convert a date to Modified Julian Date -- the fractional number of days
 %   since 00:00 UT on 1858-11-17.
 %
 % Calling Sequence:
-%   mjd = ISOdate2mjd (year, month, day)
+%   mjd = date2mjd (year, month, day)
 %   Calculate the Modified Julian Date from the numeric year, month and day.
 %
-%   mjd = ISOdate2mjd (date)
+%   mjd = date2mjd (date)
 %   Calulate the Modified Julian Date from a date string, Nx10
 %   character array, or 1xN element cell array of strings, where N
 %   represents the number of dates formatted as 'yyyy-mm-dd'.
 %
 % INPUTS
-%   YEAR:           in, required, type = double
+%   YEAR:           in, required, type = double, string
 %   MONTH:          in, required, type = double
 %   DAY:            in, required, type = double
 %
@@ -26,7 +26,7 @@
 % MATLAB release(s) MATLAB 7.12 (R2011a)
 % Required Products None
 %--------------------------------------------------------------------------
-function mjd = ISOdate2mjd (year, month, day)
+function mjd = date2mjd (year, month, day)
 
   % If only 1 argument passed, expect it to be an ISO date yyyy-mm-dd of some sort.
   if nargin == 1
